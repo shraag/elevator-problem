dd58d33e773af919cccf45e5d859a9c321d4bfe6
shraag <66220166+shraag@users.noreply.github.com>
2023-06-13 13:56:26 -0700

    changes in elevators app urls.py and main urls.py

--- elevator_system/elevators/urls.py
+++ elevator_system/elevators/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+from .views import ElevatorViewSet, RequestViewSet
+
+router = DefaultRouter()
+router.register(r'elevators', ElevatorViewSet, basename='elevator')
+router.register(r'requests', RequestViewSet, basename='request')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('elevators.urls')),  
+    path('', include(router.urls)),
 ]
