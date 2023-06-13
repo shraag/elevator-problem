e447c86d0311ccd6a85921ca59057ea04e8c2c2d
shraag <66220166+shraag@users.noreply.github.com>
2023-06-13 10:10:30 -0700

    added viewset methods in views.py

--- elevator_system/elevators/views.py
+++ elevator_system/elevators/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Elevator
+from .serializers import ElevatorSerializer
+
+class ElevatorViewSet(viewsets.ModelViewSet):
+    queryset = Elevator.objects.all()
+    serializer_class = ElevatorSerializer
+
+    @action(detail=True, methods=['post'])
+    def move_up(self, request, pk=None):
+        elevator = self.get_object()
+        elevator.move_up()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def move_down(self, request, pk=None):
+        elevator = self.get_object()
+        elevator.move_down()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def stop(self, request, pk=None):
+        elevator = self.get_object()
+        elevator.stop()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def open_door(self, request, pk=None):
+        elevator = self.get_object()
+        elevator.open_door()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def close_door(self, request, pk=None):
+        elevator = self.get_object()
+        elevator.close_door()
+        return Response(status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def add_request(self, request, pk=None):
+        elevator = self.get_object()
+        floor = request.data.get('floor')
+        if floor is not None:
+            elevator.add_request(floor)
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
