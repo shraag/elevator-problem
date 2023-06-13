b94e995d5d6d73c32e5a4d1cf187cd21f3897050
shraag <66220166+shraag@users.noreply.github.com>
2023-06-13 10:48:21 -0700

    made changes in urls.py

--- elevator_system/elevator_system/urls.py
+++ elevator_system/elevator_system/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from elevators.views import ElevatorViewSet, RequestViewSet
+
+router = DefaultRouter()
+router.register(r'elevators', ElevatorViewSet, basename='elevator')
+router.register(r'requests', RequestViewSet, basename='request')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
